d70007c502fe223d747e8fcbf1941d47314f4d01
phungthanhtu <thanhtucqtuit1952@gmail.com>
2022-10-20 19:13:02 +0700

    Add Image Service allow us to - Upload Image using API - Get Image with URL

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
 var authRouter = require('./src/routes/auth');
+var imageRouter = require('./src/routes/image');
 var mongoose = require('mongoose');
 require('dotenv').config();
 // connect to mongo
@@ -27,13 +28,14 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use('/image', imageRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));

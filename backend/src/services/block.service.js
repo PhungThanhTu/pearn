cd88f815383520f1b4d26eb1b6255fbebae8c637
phungthanhtu <thanhtucqtuit1952@gmail.com>
2022-12-18 15:57:11 +0700

    New block model

---
+++ backend/src/services/block.service.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+module.exports = {
+    httpCreateBlock: async (req,res) => {
+
+    },
+    httpGetBlock: async (req,res) => {
+
+    },
+    httpGetAllBlocks: async (req,res) => {
+
+    },
+    httpUpdateBlock: async (req,res) => {
+
+    },
+    httpDeleteBlock: async (req,res) => {
+
+    }
+
+}

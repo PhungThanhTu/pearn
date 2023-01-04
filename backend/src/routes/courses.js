9a5ea1317b3296217a4d7ffd47da30e750e78fe6
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-01-05 03:32:23 +0700

    Rating model and service

--- backend/src/routes/courses.js
+++ backend/src/routes/courses.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { authorize } = require('../middlewares/auth.middleware');
 const { httpDeleteCourse, httpCreateCourse, httpGetCourse, httpAddStudent, httpRemoveStudent, httpAddLecturer, httpRemoveLecturer, httpGetCourses } = require('../services/courses.service');
+const { httpRateCourse, httpGetAverageStar } = require('../services/rating.service');
 
 
 router.post('/',authorize("staff"),httpCreateCourse);
@@ -12,6 +13,8 @@ router.post('/addStudent',authorize("staff"),httpAddStudent);
 router.post('/deleteStudent',authorize("staff"),httpRemoveStudent);
 router.post('/addLecturer',authorize("staff"),httpAddLecturer);
 router.post('/removeLecturer',authorize("staff"),httpRemoveLecturer);
+router.post('/rate/:courseId',authorize(undefined),httpRateCourse);
+router.get('/rate/:courseId',authorize(undefined),httpGetAverageStar);
 
 module.exports = router;
 
